0c775abbaf5bfd34605f1dab920ffc20f2600702
Santiago Quinteros <contacto.quinteros@gmail.com>
2017-05-18 16:46:51 -0300

    Import app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import 'chart.js';
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
+    BrowserAnimationsModule,
+    ChartsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
