e0791cc40ee45eb4b214cf0c63aefdb5162dbf9a
Santiago Quinteros <contacto.quinteros@gmail.com>
2017-07-21 18:04:50 -0300

    Van Module working

--- src/pages/inversiones-menu/components/van/van.ts
+++ src/pages/inversiones-menu/components/van/van.ts
@@ -6,21 +6,21 @@ import { NavController } from 'ionic-angular';
 })
 
 export class VanPage {
-
-  public flujosFondo: number[];
-  public r: number;
+  
+  public flujosFondo: any[] = [{ value: 1000 },{ value: 1500 }, { value: 2000 }, { value: 2500 }];
+  public r: number = 0.1;
   public van: number;
 
   constructor(public navCtrl: NavController) {
-
+    this.doCalculation();
   }
 
   public doCalculation(): void {
-    this.van = this.flujosFondo.reduce((prev, number, index, array) => {
+    this.van = this.flujosFondo.reduce((prev, curr, index, array) => {
       if(index === 0) {
-        return number;
+        return curr.value;
       } else {
-        return prev + (number) / Math.pow((1+this.r),index);
+        return prev + (curr.value) / Math.pow((1+this.r),index);
       }
     }, 0)
   }
