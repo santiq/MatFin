8923fc4798d1b842f79203ce152402e614f598eb
Santiago Quinteros <contacto.quinteros@gmail.com>
2017-08-25 19:10:01 -0300

    Fix tiempo

--- src/pages/capitalizacion-menu/components/tiempo/tiempo.ts
+++ src/pages/capitalizacion-menu/components/tiempo/tiempo.ts
@@ -6,31 +6,33 @@ import { NavController } from 'ionic-angular';
 })
 
 export class TiempoCAPPage {
-  public convertToNumber(event): number { return +event; }
-  public Tasa = 0.1;
-  public ValorFuturo = 100000;
-  public MomentoValuacion = 3;
-  public Cuota = 12227.302;
+  public Tasa: number = 0.1;
+  public ValorFuturo: number = 100000;
+  public MomentoValuacion: number = 3;
+  public Cuota: number = 12227.302;
   public CuotaAdelantada = false;
 
   public Tiempo = 0;
 
   constructor(public navCtrl: NavController) {
-    this.doCalculation();
+    this.doCalculation(this.ValorFuturo, this.Tasa, this.Cuota, this.CuotaAdelantada);
   }
 
 
-  public doCalculation(): void {
+  public doCalculation(valorFuturo, tasa, cuota, cuotaAdelantada): void {
+    let _valorFuturo = parseFloat(valorFuturo);
+    let _tasa = parseFloat(tasa);
+    let _cuota = parseFloat(cuota);
 
-    let A = (this.ValorFuturo / ( this.Cuota * Math.pow((1 + this.Tasa), this.getExponent()) ) ) * this.Tasa + 1;
+    let A = (_valorFuturo / (_cuota * Math.pow((1 + _tasa), this.getExponent(cuotaAdelantada)) ) ) * _tasa + 1;
 
-    let B = 1 + this.Tasa;
+    let B = 1 + _tasa;
 
     this.Tiempo = Math.log(A) / Math.log(B)
   }
 
-  private getExponent() {
-    if (this.CuotaAdelantada) {
+  private getExponent(cuotaAdelantada): number {
+    if (cuotaAdelantada) {
       return 1;
     } else {
       return 0;
