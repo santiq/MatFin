c4e777b2ba87f6841ac91af4de4cb325fd86f4d0
Santiago Quinteros <contacto.quinteros@gmail.com>
2017-08-25 19:19:51 -0300

    Fix deuda

--- src/pages/financiacion-menu/components/deuda-financiacion/deuda.ts
+++ src/pages/financiacion-menu/components/deuda-financiacion/deuda.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
 })
 
 export class DeudaPageFin {
-  public convertToNumber(event): number { return +event; }
+  public convertToNumber(event): number { return parseFloat(event); }
   public Tasa = 0.1;
   public Tiempo = 10;
   public MomentoValuacion = 3;
@@ -15,20 +15,25 @@ export class DeudaPageFin {
   public Deuda = 0;
 
   constructor(public navCtrl: NavController) {
-    this.doCalculation();
+    this.doCalculation(this.Cuota, this.Tasa, this.Tiempo, this.MomentoValuacion, this.CuotaAdelantada);
   }
 
-  public doCalculation(): void {
-    let g = (1 - (Math.pow((1 + this.Tasa), -this.Tiempo))) / this.Tasa;
-    let h = Math.pow((1+this.Tasa), this.getExponent())
-    this.Deuda = this.Cuota * (g * h);
+  public doCalculation(cuota, tasa, tiempo, momentoValuacion, cuotaAdelantada): void {
+    let _cuota = parseFloat(cuota);
+    let _tasa = parseFloat(tasa);
+    let _tiempo = parseFloat(tiempo);
+    let _momentoValuacion = parseFloat(momentoValuacion);
+
+    let g = (1 - (Math.pow((1 + _tasa), -_tiempo))) / _tasa;
+    let h = Math.pow((1+_tasa), this.getExponent(_momentoValuacion, cuotaAdelantada))
+    this.Deuda = _cuota * (g * h);
   }
 
-  private getExponent() {
-    if (this.CuotaAdelantada) {
-      return this.MomentoValuacion + 1
+  private getExponent(momentoValuacion, cuotaAdelantada) {
+    if (cuotaAdelantada) {
+      return momentoValuacion + 1
     } else {
-      return this.MomentoValuacion;
+      return momentoValuacion;
     }
   }
 
