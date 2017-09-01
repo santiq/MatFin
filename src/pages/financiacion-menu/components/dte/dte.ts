db6c0af93e7455f3726b8f10d6e0b72024ee847d
Santiago Quinteros <contacto.quinteros@gmail.com>
2017-09-01 17:06:38 -0300

    Move api to provider

--- src/pages/financiacion-menu/components/dte/dte.ts
+++ src/pages/financiacion-menu/components/dte/dte.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { DTE, IDTEParams } from '../../../../api/formulas/DTE/index'
+import { DTE, IDTEParams } from '../../../../providers/determinacion-tasa-efectiva/index';
 
 @Component({
   templateUrl: 'dte.html'
